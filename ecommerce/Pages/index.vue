ff5fa9c3fc0285f8e346e636d6632fbe3a566628
riana128 <ririsa0812@gmail.com>
2023-12-01 18:29:01 +0700

    tugas-akhir

---
+++ ecommerce/Pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="hero w-full bg-gradient-to-r from-yellow-200 via-pink-300 to-purple-400 min-h-screen flex flex-col">
+    <div class="hero-content flex-col lg:flex-row mt-10">
+      <div class="w-[600px] ml-[60px]">
+        <h1 class="text-5xl font-bold text-red-800">Selamat datang di Rianaz Scincare...</h1>
+        <p class="mt-4 text-lg text-yellow-700">Pilihlah produk scincare terbaik kalian untuk memperbaiki kulit kalian.</p>
+      </div>
+    </div>
+  </div>
+</template>
